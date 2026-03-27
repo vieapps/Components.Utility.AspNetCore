7c0aa7d4f8b6d9f5c22b8e140185fabc77c2f51a
Quynh Nguyen <mr.quynh@gmail.com>
2026-03-28 01:35:59 +0700

    (refactor): remove unused code

--- Collection.cs
+++ Collection.cs
@@ -19,56 +19,6 @@ namespace net.vieapps.Components.Utility
 	public static partial class AspNetCoreCollectionService
 	{
 		/// <summary>
-		/// Adds an item into ASP.NET Core Session
-		/// </summary>
-		/// <param name="session"></param>
-		/// <param name="key"></param>
-		/// <param name="value"></param>
-		public static void Add(this ISession session, string key, object value)
-		{
-			if (!string.IsNullOrWhiteSpace(key))
-				try
-				{
-					session.Set(key.ToLower(), Helper.Serialize(value));
-				}
-				catch (Exception ex)
-				{
-					Logger.Log<ISession>(LogLevel.Debug, LogLevel.Warning, $"Cannot add object into session: {ex.Message}", ex);
-				}
-		}
-
-		/// <summary>
-		/// Gets an item from ASP.NET Core Session
-		/// </summary>
-		/// <param name="session"></param>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		public static object Get(this ISession session, string key) 
-			=> !string.IsNullOrWhiteSpace(key) && session.TryGetValue(key.ToLower(), out var value)
-				? Helper.Deserialize(value)
-				: null;
-
-		/// <summary>
-		/// Gets an item from ASP.NET Core Session
-		/// </summary>
-		/// <param name="session"></param>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		public static T Get<T>(this ISession session, string key) 
-			=> !string.IsNullOrWhiteSpace(key) &&  session.TryGetValue(key.ToLower(), out var value)
-				? Helper.Deserialize<T>(value)
-				: default;
-
-		/// <summary>
-		/// Checks to see the key is existed in ASP.NET Core Session or not
-		/// </summary>
-		/// <param name="session"></param>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		public static bool ContainsKey(this ISession session, string key) 
-			=> !string.IsNullOrWhiteSpace(key) && session.Keys.FirstOrDefault(k => k.IsEquals(key.ToLower())) != null;
-
-		/// <summary>
 		/// Sets an object into this context items
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
